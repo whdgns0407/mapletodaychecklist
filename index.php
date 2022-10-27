c4f7fb218e0c937eadbad43a1cd0ab9d8560f5fd
whdgns0407 <whdgns0407@gmail.com>
2022-10-27 23:19:58 +0900

    메할일 체크리스트 2022-10-13 패치 이후

---
+++ index.php
@@ -0,0 +1,164 @@
+﻿<?php include 'layout/업체.php' ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>메할일 체크리스트 - 메할일.com</title>
+		<?php include 'layout/header.php' ?>
+		<meta property="og:type" content="website"> 
+		<meta property="og:title" content="메할일 체크리스트">
+		<meta property="og:description" content="메할일 체크리스트 / 메이플 끄기전 확인해야할 리스트">
+		<meta property="og:image" content="https//메할일.com/img/Odium.jpg">
+		<meta property="og:url" content="https//메할일.com">
+		
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="main">
+				<div class="inner">	
+				
+					<section>
+						<header class="main">
+							<h1 id="content">메할일 체크리스트</h1>
+						</header>
+						<?php include 'layout/banner.php' ?>
+						<h2>2022-10-13 헤이스트 이후 적용 완료</h2>
+						<?php include 'layout/mainad.php' ?>
+						
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/황금마차.png" alt="황금마차" /></span>
+								<h2>페어리 브로의 황금마차</h2>
+								1시간 접속 후 황금마차 보상을 수령하였는지 확인해보세요<br>
+								출석하지 않으면 3,000메포가 나갑니다!!<br><br><hr>
+							</p>
+						</div>	
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/super booster.jpg" alt="헤이스트 슈퍼 부스터" /></span>
+								<h2>사냥 가속 헤이스트</h2>
+								헤이스트 미션 완료, 보상 수령, 부스터 여부 확인<br>
+								<br><br><hr>
+							</p>
+						</div>	
+						
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/펀치킹.jpg" alt="펀치킹" /></span>
+								<h2>펀치킹</h2>
+								펀치킹 치시고 보상 수령 여부 확인 해보세요<br>
+								<br><br><hr>
+							</p>
+						</div>	
+					
+					
+						
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/Odium.jpg" alt="오디움" /></span>
+								<h2>오디움 일일퀘스트</h2>
+								퀘스트가 진행 중인지 퀘스트 완료 후 보상을 수령하였는지 확인해 보세요.<br><br><hr>
+							</p>
+						</div>	
+					
+					
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/hotel.jpg" alt="호텔 아르크스" /></span>
+								<h2>호텔 아르크스 일일퀘스트</h2>
+								퀘스트가 진행 중인지 퀘스트 완료 후 보상을 수령하였는지 확인해 보세요.<br><br><hr>
+							</p>
+						</div>		
+
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/Hurnium.jpg" alt="후르니움" /></span>
+								<h2>후르니움 일일퀘스트</h2>
+								퀘스트가 진행 중인지 퀘스트 완료 후 보상을 수령하였는지 확인해 보세요.<br><br><hr>
+							</p>
+						</div>		
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/Cernium.jpg" alt="세르니움" /></span>
+								<h2>세르니움 일일퀘스트</h2>
+								퀘스트가 진행 중인지 퀘스트 완료 후 보상을 수령하였는지 확인해 보세요.<br><br><hr>
+							</p>
+						</div>		
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/urus.jpg" alt="파왕 우르스" /></span>
+								<h2>우르스</h2>
+								하루에 3번 그이상 도전시 미친짓이라는 우르스 메소 받으셨나요?<br><br><br><hr>
+							</p>
+						</div>
+					
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/Arkane River Quick Pass.jpg" alt="아케인리버 퀵패스" /></span>
+								<h2>아케인리버 퀵패스 + 문브릿지 일퀘</h2>
+								아케인리버 퀵패스를 열어 아케인리버퀘스트가 완료되었는지 확인해 보세요.<br><br>
+								문브릿지 일일퀘스트 경험치 보상수령여부도 확인해 보세요.<hr>
+							</p>
+						</div>	
+
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/today boss.jpg" alt="일일보스" /></span>
+								<h2>일일보스</h2>
+								일일보스를 도시는분들은 일일보스가 클리어되었는지 확인해보세요.<br><br><hr>
+							</p>
+						</div>		
+						
+						<div class="6u 12u$(small)">
+							<p>
+								<span class="image left"><img src="img/monster.gif" alt="몬스터파크" /></span>
+								<h2>몬파(익스포함)</h2>
+								몬스터파크 완료횟수와 익스트림 몬스터파크를 클리어했는지 확인해 보세요.<br><br><hr>
+							</p>
+						</div>						
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/skill.gif" alt="명장 이펙트" /></span>
+								<h2>명장 유지</h2>
+								명장 숙련도 : 160,000 유지중인지 확인해 주세요.<br><br><br><hr>
+							</p>
+						</div>						
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/monsterlife.jpg" alt="몬스터라이프" /></span>
+								<h2>몬라 코인수령</h2>
+								몬스터라이프 코인수령 여부를 확인해 주세요.<br><br><br><hr>
+							</p>
+						</div>						
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/union.jpg" alt="유니온" /></span>
+								<h2>유니온 코인수령</h2>
+								유니온 코인수령 여부를 확인해 주세요.<br><br><br><hr>
+							</p>
+						</div>						
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/마일리지.png" alt="메이플 마일리지" /></span>
+								<h2>마일리지 적립</h2>
+								접속 종료전 보스, 소비창, 메이플m의 마일리지를 적립 후 캐시샵을 입장해 주세요.<br><br><br><hr>
+							</p>
+						</div>						
+						<div class="6u 12u$(small)">	
+							<p>
+								<span class="image left"><img src="img/guild.png" alt="길드 출석체크 창" /></span>
+								<h2>길드 출석 체크</h2>
+								접속 종료전 길마의 사랑을 받는 길드 출석 체크 하셨나요?<br><br><br><hr>
+							</p>
+						</div>
+						
+					</section> 
+				</div>	
+			</div>
+			<div id="sidebar">
+				<?php
+					include 'layout/left-menu.php';
+				?>
+			</div>
+		</div>
+	</body>
+</html>
